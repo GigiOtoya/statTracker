1ac42ae11f3db8c82c65a3d5bdfc57806d94215b
Luigi Otoya <Luigi.Otoya@gmail.com>
2024-03-04 15:41:32 -0500

    moved header button to user component

--- client/src/components/header/Header.tsx
+++ client/src/components/header/Header.tsx
@@ -1,20 +1,11 @@
-import { SignedIn, SignedOut, useUser, useAuth } from "@clerk/clerk-react";
+import { SignedIn, SignedOut } from "@clerk/clerk-react";
 import { Link } from "react-router-dom";
-import { ActionButton } from "../actionButton/ActionButton";
-import { BiSolidUserDetail } from "react-icons/bi";
 import { UserOptions } from "../userOptions/UserOptions";
 import { useState } from "react";
 import styles from "./Header.module.css";
 
 export const Header = () => {
   const [showUserOptions, setShowUserOptions] = useState(false);
-  const { user } = useUser();
-
-  if (!user) {
-    return null;
-  }
-
-  const email = user.emailAddresses[0].emailAddress;
 
   const handleOnClick = () => {
     setShowUserOptions(!showUserOptions);
@@ -27,13 +18,7 @@ export const Header = () => {
           <Link to="/">Squad Builder</Link>
         </div>
         <SignedIn>
-          <ActionButton
-            text={email ?? "user"}
-            className="btn-clear large"
-            icon={<BiSolidUserDetail fontSize={"24px"} />}
-            onClick={handleOnClick}
-          />
-          {showUserOptions && <UserOptions user={user}>1</UserOptions>}
+          <UserOptions />
         </SignedIn>
         <SignedOut>
           <div className={styles.action}>
--- client/src/components/userOptions/UserOptions.tsx
+++ client/src/components/userOptions/UserOptions.tsx
@@ -1,30 +1,17 @@
-import { ReactNode, useRef, useState } from "react";
+import { useRef, useState } from "react";
 import { useDetectOutside } from "../../hooks/useDetectOutside";
-import styles from "./UserOptions.module.css";
-import { FaUserCircle } from "react-icons/fa";
-import { FiLogOut } from "react-icons/fi";
 import { SignOutButton, useUser } from "@clerk/clerk-react";
-import {} from "@clerk/clerk-react";
 import { Link } from "react-router-dom";
+import { ActionButton } from "../actionButton/ActionButton";
+import { FiLogOut } from "react-icons/fi";
+import { FaUserCircle } from "react-icons/fa";
+import { BiSolidUserDetail } from "react-icons/bi";
+import styles from "./UserOptions.module.css";
 
-interface Props {
-  children: ReactNode;
-  user: NonNullable<ReturnType<typeof useUser>["user"]>;
-}
-
-export const UserOptions = ({ children, user }: Props) => {
+export const UserOptions = () => {
   const ref = useRef<HTMLDivElement>(null);
-  const [visible, setVisible] = useState<boolean>(false);
-
-  const handleOnClick = (event: React.MouseEvent) => {
-    event.stopPropagation();
-    setVisible(!visible);
-    console.log("clicked");
-  };
-
-  const handleClickOutisde = () => {
-    setVisible(false);
-  };
+  const { user, isSignedIn } = useUser();
+  const [showOptions, setShowOptions] = useState<boolean>(false);
 
   useDetectOutside({
     ref: ref,
@@ -33,30 +20,55 @@ export const UserOptions = ({ children, user }: Props) => {
     },
   });
 
+  if (!isSignedIn) {
+    return null;
+  }
+
+  const handleOnClick = () => {
+    setShowOptions(!showOptions);
+  };
+
+  const handleClickOutisde = () => {
+    setShowOptions(false);
+  };
+
   return (
-    <div className={styles.container}>
-      <div className={styles.row}>
-        <span>
-          <FaUserCircle size={50} />
-        </span>
-        <span>
-          <p>{user.fullName}</p>
-          <p>{user.emailAddresses[0].emailAddress}</p>
-        </span>
-      </div>
-      <div className={styles.row}>
-        <span>Profile</span>
-      </div>
-      <div className={styles.row}>
-        <span>
-          <FiLogOut size={24} />
-        </span>
-        <span>
-          <SignOutButton>
-            <Link to="/">Sign Out</Link>
-          </SignOutButton>
-        </span>
-      </div>
+    <div ref={ref}>
+      <ActionButton
+        text={user.emailAddresses[0].emailAddress}
+        className="btn-clear large"
+        icon={<BiSolidUserDetail fontSize={"24px"} />}
+        onClick={handleOnClick}
+      />
+
+      {showOptions && (
+        <div className={styles.container}>
+          <div className={styles.row}>
+            <span>
+              <FaUserCircle size={50} />
+            </span>
+            <span>
+              <p>{user.fullName}</p>
+              <p>{user.emailAddresses[0].emailAddress}</p>
+            </span>
+          </div>
+          <div className={styles.row}>
+            <span>Profile</span>
+          </div>
+          <div className={styles.row}>
+            <SignOutButton>
+              <Link to="/">
+                <span>
+                  <span>
+                    <FiLogOut size={24} />
+                  </span>
+                  <span>Sign Out</span>
+                </span>
+              </Link>
+            </SignOutButton>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
