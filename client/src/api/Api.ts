88d51a364dbd1a8d9cfce7bc675a57a9721da072
Luigi Otoya <Luigi.Otoya@gmail.com>
2024-04-08 15:31:53 -0400

    server

--- client/src/api/Api.ts
+++ client/src/api/Api.ts
@@ -3,29 +3,8 @@ import axios from "axios";
 type tokenFunction = () => Promise<string | null>;
 
 export const apiClient = axios.create({
-  baseURL: "http://localhost:8080/api",
+  // baseURL: "http://localhost:8080/api",
+  baseURL: "https://stat-tracker-server.onrender.com/api",
 });
 
 export default apiClient;
-
-// const apiClient = axios.create({
-//   baseURL: "http://localhost:8080/api",
-// });
-
-// export const configToken = async (getToken: tokenFunction) => {
-//   const sessionToken = await getToken();
-//   apiClient.interceptors.request.use(
-//     (config) => {
-//       if (sessionToken) {
-//         config.headers.Authorization = `Bearer ${sessionToken}`;
-//         console.log(config.headers);
-//       }
-//       return config;
-//     },
-//     (error) => {
-//       return Promise.reject(error);
-//     }
-//   );
-// };
-
-// export default apiClient;
